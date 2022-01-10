b3750716b1f0472354745e876957ef7c3e2d829e
Ioan-Samuel Terec <terec_ioan@yahoo.com>
2022-01-10 12:58:13 +0200

    Add project files.

---
+++ Terec_Ioan_Lab12/Terec_Ioan_Lab12/Terec_Ioan_Lab12/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Terec_Ioan_Lab12.Models;
+
+namespace Terec_Ioan_Lab12.Data
+{
+  
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+        }
+    
+}
